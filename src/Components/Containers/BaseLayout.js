543aa8aa47ec2ff6915974fe1f35278b13b5b5de
Orangel Brito <orangeleando@gmail.com>
2023-08-11 18:24:36 -0500

    deploy1

---
+++ src/Components/Containers/BaseLayout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useMachine } from '@xstate/react';
+import { Nav } from '../Nav';
+import { StepsLayout } from './StepsLayout';
+import bookingMachine from '../../Machines/bookingMachine';
+import './BaseLayout.css';
+
+export const BaseLayout = ()=> {
+  const [state,send] = useMachine(bookingMachine)
+
+  console.log('our machine', state.value, state.context)
+  return (
+      <div className='BaseLayout'>
+        <Nav state ={state} send={send}/>
+        <StepsLayout state ={state} send={send}/>
+      </div>
+  )
+}
