015fcb0a35b8c6b45458f6181d38fe92c94d3f2f
Orangel Brito <orangeleando@gmail.com>
2023-08-11 18:38:29 -0500

    [FIX bug on country]

--- src/Components/Tickets.js
+++ src/Components/Tickets.js
@@ -10,7 +10,7 @@ export const Tickets = ({ send, context }) => {
     <div className='Tickets'>
       <p className='Tickets-description description'>Gracias por volar con book a fly 💚</p>
       <div className='Tickets-ticket'>
-        <div className='Tickets-country'>Colombia</div>
+        <div className='Tickets-country'>{context.selectedCountry}</div>
         <div className='Tickets-passengers'>
           <span>✈</span>
           {context.passengers.map((person, idx) => {
